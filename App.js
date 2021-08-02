0a2bf590722dd60de77a95cb6a9ea902ca9362ba
liukefu <liukefu2020@sina.com>
2021-08-02 12:08:09 +0800

    init

--- App.js
+++ App.js
@@ -1,114 +1,90 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
+ * 主界面
  */
-
-import React from 'react';
+'use strict';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
+  Alert,
+  TouchableOpacity,
   Text,
-  StatusBar,
+  View,
+  StyleSheet,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import ReactNativeCustomizedUpdate from 'react-native-customized-update'
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      version:"",
+      cacheSize:""
+    }
+  }
+
+  componentDidMount() {
+    let that = this;
+
+    ReactNativeCustomizedUpdate.shouldApkUpdate({})
+
+    ReactNativeCustomizedUpdate.getAppVersion(function (value) {
+      console.log(value)
+      that.setState({version:value});
+    })
+
+    ReactNativeCustomizedUpdate.getAppCacheSize(function (value, unit) {
+      console.log(value)
+      console.log(unit)
+
+      that.setState({cacheSize:value+unit});
+    })
+  }
+
+
+  _cleanCache(){
+    //清除缓存
+    ReactNativeCustomizedUpdate.clearAppCache(function () {
+
+      //that.setState({cacheSize:''});
+    })
+  }
+
+
+
+  render() {
+    return (
+        <View style={{marginTop:50,alignItems:'center'}}>
+          <Text>================main page================</Text>
+          <Text>version:{this.state.version}</Text>
+          <Text>cache:{this.state.cacheSize}</Text>
+
+          <TouchableOpacity
+
+              onPress={() => {
+                 this._cleanCache()
+              }}
+          >
+            <View style={{marginTop:50}}>
+              <Text>clear cache</Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </TouchableOpacity>
+        </View>
+    );
+  }
+
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  iconStyle: {
+    width: 23,
+    height: 23,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  textStyle: {
+    color: '#999',
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  selectedTextStyle: {
+    color: 'black',
+  }
 });
 
-export default App;
+export default App
